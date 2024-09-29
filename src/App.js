5c84cf9e1f8043e6a2197005c5a221ae628c4dbb
Jonathan Delgado <jonathan.degado6@gmail.com>
2024-09-29 15:29:46 -0600

    imguno

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { Formik, Form, Field } from 'formik'
+import './header.css'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <header>
+        <Formik
+        initialValues = {{ search: '' }}
+        onSumit={async value=>{
+          //llamar api de unsplash
+          console.log(value)
+        }}
         >
-          Learn React
-        </a>
+          <Form>
+            <Field name="search"/>
+          </Form>
+        </Formik>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
